167b628b21bb87897cc9afd9c1132041285d10ae
mBruksaas <martin_4144@hotmail.com>
2015-03-05 18:23:26 +0100

    v0.2

--- Oblig 3 Oppg 3 Del 2/src/TowersOfHanoi.java
+++ Oblig 3 Oppg 3 Del 2/src/TowersOfHanoi.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
 public class TowersOfHanoi {
-
+	static int a = 0;
+	
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Tast inn antall disker: ");
@@ -10,13 +11,15 @@ public class TowersOfHanoi {
 		
 		System.out.println("Trekkene er:");
 		moveDisks(n, 'A', 'B', 'C');
+		System.out.println("Antall trekk: " + a);
 	}
 	
 	public static void moveDisks(int n, char fromTower, char toTower, char auxTower) {
-		
 		if(n == 1) {
+			a++;
 			System.out.println("Flytt disk " + n + " fra " + fromTower + " til " + toTower);
 		} else {
+			a++;
 			moveDisks(n-1, fromTower, auxTower, toTower);
 			System.out.println("Flytt disk " + n + " fra " + fromTower + " til " + toTower);
 			moveDisks(n - 1, auxTower, toTower, fromTower);
