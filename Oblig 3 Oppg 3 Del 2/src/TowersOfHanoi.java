6b48548b734f5a182369b629ebb29dfea03ec80f
mBruksaas <martin_4144@hotmail.com>
2015-03-05 18:34:08 +0100

    v0.3

--- Oblig 3 Oppg 3 Del 2/src/TowersOfHanoi.java
+++ Oblig 3 Oppg 3 Del 2/src/TowersOfHanoi.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class TowersOfHanoi {
 	static int a = 0;
+	static int b = 0;
 	
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
@@ -11,10 +12,15 @@ public class TowersOfHanoi {
 		
 		System.out.println("Trekkene er:");
 		moveDisks(n, 'A', 'B', 'C');
+		
+		//Litt merkelig å skulle måte lage to separate tellere,
+		//da en disk flyttes ved hvert kall uansett
 		System.out.println("Antall trekk: " + a);
+		System.out.println("Antall kall til moveDisks: " + b);
 	}
 	
 	public static void moveDisks(int n, char fromTower, char toTower, char auxTower) {
+		b++;
 		if(n == 1) {
 			a++;
 			System.out.println("Flytt disk " + n + " fra " + fromTower + " til " + toTower);
